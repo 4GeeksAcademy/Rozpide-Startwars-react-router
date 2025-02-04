6e181c287a2bd51f48e6577c874db6b8fa70d01c
Rozpi <100348581+Rozpide@users.noreply.github.com>
2025-02-04 19:06:29 +0000

    ajusto la vista individual de la tarjeta elegida con bootstrap

--- src/js/views/newDetails.js
+++ src/js/views/newDetails.js
@@ -74,7 +74,7 @@ const NewDetails = () => {
 
 export { NewDetails };*/
 /*----------------------------------------------*/
-import React, { useContext, useEffect, useState } from "react";
+/*import React, { useContext, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { Context } from "../store/appContext";
 
@@ -139,5 +139,123 @@ const NewDetails = () => {
   );
 };
 
+export { NewDetails };*/
+/*----------------------------------------------*/
+import React, { useContext, useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { Context } from "../store/appContext";
+
+const NewDetails = () => {
+  const { store } = useContext(Context);
+  const { category, id } = useParams();
+  const [details, setDetails] = useState(null);
+  const [imageUrl, setImageUrl] = useState("");
+
+  useEffect(() => {
+    // Buscar los detalles en el store según la categoría y el ID
+    const item = 
+      category === "people"
+        ? store.people.find(p => p.url.split('/').slice(-2, -1)[0] === id)
+        : category === "planets"
+        ? store.planets.find(pl => pl.url.split('/').slice(-2, -1)[0] === id)
+        : null;
+
+    setDetails(item);
+
+    // Generar la URL de la imagen
+    const imageUrl = `https://starwars-visualguide.com/assets/img/${category}/${id}.jpg`;
+    setImageUrl(imageUrl);
+  }, [category, id, store]);
+
+  if (!details) return <div>Loading...</div>;
+
+  return (
+    <div className="container mt-5">
+      <div className="card w-100">
+        <div className="card-body d-flex">
+          <div className="img-container" style={{ width: "600px", height: "400px" }}>
+            <img src={imageUrl} className="img-fluid" alt={details.name || details.title} />
+          </div>
+          <div className="details-container ms-3">
+            <h1>{details.name || details.title}</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur scelerisque vehicula nulla, a fermentum magna vestibulum et. Nulla facilisi. Quisque bibendum semper risus, in scelerisque purus ullamcorper in. Sed at sem vehicula, scelerisque ex vel, consequat urna.</p>
+          </div>
+        </div>
+        <div className="card-footer d-flex flex-wrap">
+          {category === "people" && (
+            <>
+              <div className="prop-container me-4">
+                <h5>Height</h5>
+                <p>{details.height}</p>
+              </div>
+              <div className="prop-container me-4">
+                <h5>Mass</h5>
+                <p>{details.mass}</p>
+              </div>
+              <div className="prop-container me-4">
+                <h5>Hair Color</h5>
+                <p>{details.hair_color}</p>
+              </div>
+              <div className="prop-container me-4">
+                <h5>Skin Color</h5>
+                <p>{details.skin_color}</p>
+              </div>
+              <div className="prop-container me-4">
+                <h5>Eye Color</h5>
+                <p>{details.eye_color}</p>
+              </div>
+              <div className="prop-container me-4">
+                <h5>Birth Year</h5>
+                <p>{details.birth_year}</p>
+              </div>
+              <div className="prop-container me-4">
+                <h5>Gender</h5>
+                <p>{details.gender}</p>
+              </div>
+            </>
+          )}
+          {category === "planets" && (
+            <>
+              <div className="prop-container me-4">
+                <h5>Climate</h5>
+                <p>{details.climate}</p>
+              </div>
+              <div className="prop-container me-4">
+                <h5>Diameter</h5>
+                <p>{details.diameter}</p>
+              </div>
+              <div className="prop-container me-4">
+                <h5>Gravity</h5>
+                <p>{details.gravity}</p>
+              </div>
+              <div className="prop-container me-4">
+                <h5>Orbital Period</h5>
+                <p>{details.orbital_period}</p>
+              </div>
+              <div className="prop-container me-4">
+                <h5>Population</h5>
+                <p>{details.population}</p>
+              </div>
+              <div className="prop-container me-4">
+                <h5>Rotation Period</h5>
+                <p>{details.rotation_period}</p>
+              </div>
+              <div className="prop-container me-4">
+                <h5>Surface Water</h5>
+                <p>{details.surface_water}</p>
+              </div>
+              <div className="prop-container me-4">
+                <h5>Terrain</h5>
+                <p>{details.terrain}</p>
+              </div>
+            </>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
 export { NewDetails };
+
 
